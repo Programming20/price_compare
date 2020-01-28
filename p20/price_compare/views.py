e83c13e81e6ee043e5eeae1c5408bdee2b05d2b7
Noah Rozelle <nrozelle@uncc.edu>
2020-01-27 19:25:32 -0500

    Added bootstrap template and configured first view for home page

--- p20/price_compare/views.py
+++ p20/price_compare/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 
 # Create your views here.
+def home(request):
+    title = 'Home'
+    return render(request, 'price_compare/base.html', {'title': title})
